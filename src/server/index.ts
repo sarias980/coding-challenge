de16df4f7ac2cdeb90e464cc2647c3dcf4000715
Sarias <53831285s@iespoblenou.org>
2022-05-20 16:14:14 +0200

    beynd: Defining endpoint and basic structure of the api

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,31 @@
-export default async function main() {
-  // something here
-}
+import express, {Request, Response} from "express";
+
+// ENV file
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// GET - Return all the influencers
+app.get('/influencers', (req: Request, res: Response) => {
+    console.log('GET - Return all the influencers')
+    return
+})
+
+// GET - Return the top 1 for category
+app.get('/influencers/top/category/:category', (req: Request, res: Response) => {
+    console.log('GET - Return the top 1 for category')
+    return
+})
+
+// GET - Return the top 1 for country
+app.get('/influencers/top/country/:country', (req: Request, res: Response) => {
+    console.log('GET - Return the top 1 for country')
+    return
+})
+
+//set the server with the specific port
+app.listen(port, () => {
+    console.log(`Server listening at port ${port}`)
+})
---
+++ src/server/models/influencer.modal.ts
@@ -0,0 +1,10 @@
+export class Influencer {
+    name: string;
+    ig_name: string;
+    category_1: string;
+    category_2: string;
+    followers: string;
+    audienceCountry: string;
+    authenticEngagement: string;
+    engagementAvg: string;
+}
